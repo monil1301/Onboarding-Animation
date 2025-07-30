08b43cbf410d3ecbb978d180a55b25980cf7e416
monil1301 <70565653+monil1301@users.noreply.github.com>
2025-07-30 23:47:56 +0530

    Added hilt dependency and created Application file

---
+++ app/src/main/java/com/shah/onboardinganimations/OnboardingAnimationsApp.kt
@@ -0,0 +1,11 @@
+package com.shah.onboardinganimations
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Monil on 30/07/25.
+ */
+
+@HiltAndroidApp
+class OnboardingAnimationsApp : Application()
