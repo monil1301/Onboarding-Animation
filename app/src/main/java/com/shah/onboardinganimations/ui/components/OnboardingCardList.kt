1305271650bc0c2b8150c9bef9a9b335769e5998
monil1301 <70565653+monil1301@users.noreply.github.com>
2025-08-01 14:23:41 +0530

    Dummy data for cards to test

--- app/src/main/java/com/shah/onboardinganimations/ui/components/OnboardingCardList.kt
+++ app/src/main/java/com/shah/onboardinganimations/ui/components/OnboardingCardList.kt
@@ -21,6 +21,12 @@ import androidx.compose.ui.unit.dp
 fun OnboardingCardList() {
     var expandedIndex by remember { mutableIntStateOf(-1) } // -1 means none expanded
 
+    val imageUrls = listOf(
+        "https://img.myjar.app/RJf1PnhlAQ_5oZcgHo-g3I__7nIsAqFWoCj3-4dq_Hs/rs:fit:0:0/q:60/format:webp/plain/https://cdn.myjar.app/Homefeed/engagement_card/buyGoldEducation1.webp",
+        "https://img.myjar.app/yXXRNLKGxWkoWRyyzurnZr3UJNHbqVaCHiLk1VYGlDM/rs:fit:0:0/q:60/format:webp/plain/https://cdn.myjar.app/Homefeed/engagement_card/buyGoldEducation2.webp",
+        "https://img.myjar.app/fLqaeBj3sX4VtpjdRrj2_HxHPJ4YmqaFX9jPcm563cc/rs:fit:0:0/q:60/format:webp/plain/https://cdn.myjar.app/Homefeed/engagement_card/buyGoldEducation3.webp",
+    )
+
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(vertical = 8.dp)
@@ -31,7 +37,9 @@ fun OnboardingCardList() {
                 onCardClick = {
                     expandedIndex = if (expandedIndex == index) -1 else index
                 },
-                title = "Card ${index + 1}"
+                collapsedText = "Card ${index + 1}",
+                expandedText = "Expanded Card ${index + 1}",
+                imageUrl = imageUrls[index]
             )
             Spacer(modifier = Modifier.height(12.dp))
         }
