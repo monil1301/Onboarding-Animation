7fff6c120dba8061b79449cb82e095872030e523
monil1301 <70565653+monil1301@users.noreply.github.com>
2025-08-01 15:00:58 +0530

    Added onboarding api class

---
+++ app/src/main/java/com/shah/onboardinganimations/data/api/OnboardingApi.kt
@@ -0,0 +1,23 @@
+package com.shah.onboardinganimations.data.api
+
+import com.shah.onboardinganimations.data.model.OnboardingResponse
+import com.shah.onboardinganimations.utils.Constants
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import javax.inject.Inject
+
+/**
+ * Created by Monil on 01/08/25.
+ */
+
+class OnboardingApi @Inject constructor(
+    private val client: HttpClient
+) {
+    suspend fun getOnboardingDetails(): OnboardingResponse =
+        client.post(Constants.API.PATH.ONBOARDING) {
+            contentType(ContentType.Application.Json)
+        }.body()
+}
---
+++ app/src/main/java/com/shah/onboardinganimations/utils/Constants.kt
@@ -0,0 +1,15 @@
+package com.shah.onboardinganimations.utils
+
+/**
+ * Created by Monil on 01/08/25.
+ */
+
+object Constants {
+    object API {
+        private const val BASE_URL = "https://api.npoint.io/"
+
+        object PATH {
+            const val ONBOARDING = BASE_URL + "796729cca6c55a7d089e"
+        }
+    }
+}
